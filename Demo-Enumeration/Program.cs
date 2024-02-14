f40f83015869ee3430527a7fc4435c27acb508b1
Samuel Legrain <samuel.legrain@bstorm.be>
2024-02-14 17:02:21 +0100

    Ajout Démonstration Enumération

---
+++ Demo-Enumeration/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Demo_Enumeration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FeuRouge f1 = new FeuRouge();
+            f1.PeutPasser = false;
+            f1.couleur = CouleurFeuRouge.Rouge;
+
+            f1.ChangerCouleur();
+
+            Console.Write("Mon feu rouge est de couleur ");
+            switch (f1.couleur)
+            {
+                case CouleurFeuRouge.Vert:
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    break;
+                case CouleurFeuRouge.Orange:
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    break;
+                case CouleurFeuRouge.Rouge:
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    break;
+            }
+
+            Console.WriteLine(f1.couleur);
+            Console.ResetColor();
+
+            if (f1.PeutPasser)
+            {
+                Console.WriteLine("J'avance!");
+            }
+            else
+            {
+                Console.WriteLine("Je m'arrête!");
+            }
+        }
+    }
+}
---
+++ Demo-Enumeration/Structs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Enumeration
+{
+    public struct FeuRouge
+    {
+        public bool PeutPasser;
+        public CouleurFeuRouge couleur;
+
+        public void ChangerCouleur()
+        {
+            /* méthode simple à comprendre, mais compliqué à mettre en place...
+            if(couleur == CouleurFeuRouge.Rouge)
+            {
+                couleur = CouleurFeuRouge.Vert;
+                PeuPasser = true;
+            }
+            else if(couleur == CouleurFeuRouge.Orange)
+            {
+                couleur = CouleurFeuRouge.Rouge;
+                PeuPasser = false;
+            }
+            else if(couleur == CouleurFeuRouge.Vert)
+            {
+                couleur = CouleurFeuRouge.Orange;
+                PeuPasser = false;
+            }*/
+
+            couleur = (CouleurFeuRouge)(((int)couleur + 1) % 3);
+            PeutPasser = couleur == 0;
+        }
+    }
+}
